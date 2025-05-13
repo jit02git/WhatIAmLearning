eb8895fa3e25f3d9db183f8edf82c64cc24583b3
jitesh <jitesh02sep@gmail.com>
2025-05-13 22:27:15 +0530

    start the basic of JS part-1

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("this is starting of my learning journey");
+console.log('1. Start');
+
+setTimeout(() => {
+  console.log('2. Callback from setTimeout');
+}, 0);
+
+console.log('3. Synchronous log');
